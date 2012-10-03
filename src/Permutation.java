6356927e39a5b871f271ff99bd7607638d4e5b20
chandershivdasani <chandershivdasani@gmail.com>
2012-10-02 22:02:17 -0700

    A new way to compute permutation

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,56 +1,83 @@
-
 /**
- * Compute all the permutations of the given string.
- * Reference: http://newton.ex.ac.uk/teaching/jmr/recursion.html
+ * Compute all the permutations of the given string. Reference:
+ * http://newton.ex.ac.uk/teaching/jmr/recursion.html
  * 
  * Total number of permutations for a string of len(n): n!
  * 
  * @author Chander
- *
+ * 
  */
 public class Permutation {
 
-	/**
-	 * The String out here is represented as 
-	 * a char array for simplicity. This program can 
-	 * be easily modified to use a String instead of char array.
-	 * 
-	 * @param inputString
-	 * @param p
-	 */
-	void permute(char[] inputString, int p)
-	{
-		if((p+1) == inputString.length)
-			System.out.println(inputString);
-		else 
-		{
-			/* Traverse the string, swapping each element 
-			 * in turn with element at p
-			 */
-			char temp;
-			for(int i = p; i < inputString.length; i++)
-			{
-				temp = inputString[i];
-				inputString[i] = inputString[p];
-				inputString[p] = temp;
-				
-				permute(inputString, p+1);
-				
-				//Restore the original text
-				inputString[p] = inputString[i];
-				inputString[i] = temp;
- 			}
+	public static char[] swap(char[] input, int i, int j) {
+		char temp;
+		temp = input[i];
+		input[i] = input[j];
+		input[j] = temp;
+
+		return input;
+
+	}
+	
+	public static void permuteString(String soFar, String rest) {
+		if(rest.equals(""))
+			System.out.println(soFar);
+		else{
+			for(int i = 0; i < rest.length(); i++) {
+				String next = soFar + rest.charAt(i);
+				String remaining = rest.substring(0,i) + rest.substring(i+1);
+				permuteString(next, remaining);
+			}
 		}
 	}
 	
-	public static void main(String[] args) 
-	{
-		char[] input = {'A','B','C','D','E','F'};
-		Permutation p = new Permutation();
-		long before = System.currentTimeMillis();
-		p.permute(input,0);
-		long after = System.currentTimeMillis();
-		System.out.println("Time : " + (after - before) + "ms");
+	/**
+	 * Problem formulation: Assume at a given step of recursion, we can come up
+	 * with the following generalization:
+	 * 
+	 * if close < open : INVALID, since we cannot have more open brackets than
+	 * close close == open : PRINT the string, since we are not left with any
+	 * brackets open - 1, close : if open > 0 try a open parenthesis and recurse
+	 * on remaining. open, close - 1 if close > 0 try a close parenthesis and
+	 * recurse on remaining
+	 * 
+	 * @param inputString
+	 * @param close
+	 *            - close brackets
+	 * @param open
+	 *            - open brackets
+	 */
+	public static void permuteParanthesis(StringBuilder inputString, int close,
+			int open) {
+		if (close < open)
+			return;
+
+		if (close == open)
+			System.out.println(inputString);
+
+		if (open > 0)
+			permuteParanthesis(inputString.append("{"), close, open - 1);
+		if (close > 0 && close > open)
+			permuteParanthesis(inputString.append("}"), close - 1, open);
+	}
+
+	public static void main(String[] args) {
+//		char[] input = { 'A', 'B' , 'A'};
+//		
+//		boolean[] visited = new boolean[255];
+//		
+//		for(int i = 0; i < visited.length; i++) {
+//			visited[i] = false;
+//		}
+//		
+//		// char[] input = {'A','A'};
+//		long before = System.currentTimeMillis();
+//		//permuteStrings(input,visited, 0, input.length - 1);
+//		 permuteParanthesis(new StringBuilder(""), 2, 2);
+//		long after = System.currentTimeMillis();
+//		System.out.println("Time : " + (after - before) + "ms");
 		
+		permuteString("","ABC");
+
 	}
 }
